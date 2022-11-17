3f557ddbf6dd59b8a9fbbe0806dc32f1f72c9d96
dannyrg95 <dannyrg-@hotmail.com>
2022-11-17 07:17:36 -0600

    Form Ubicacion
    Se agrega ventana Ubicación, pendiente enlazar Btn y mostrar en ventana Google Maps.

---
+++ ProyectoLenguajes/ProyectoLenguajes/Ubicacion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+Imports System.Text;
+
+namespace ProyectoLenguajes
+{
+    public partial class Ubicacion : Form
+        
+    {
+        public Ubicacion()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+            Dim postcode As String = String.Empty
+            Dim stringSearch As New StringBuilder
+
+            stringSearch.append("http://www.google.com/maps")
+            If txtSearch.Text <> String.Empty Then
+                postcode = txtSearch.Text.Replace("","+")
+                stringSearch.Append(postcode + "," & "+")
+            End If
+
+            WebBrowser.Navigate(stringSearch.ToString)
+        {
+
+        }
+    }
+}
