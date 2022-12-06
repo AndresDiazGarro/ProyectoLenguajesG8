05c3eb978c27bc6dc6b2e29e39894b5d4c19aa14
chrisaguero <chris23298@gmail.com>
2022-12-05 20:36:21 -0600

    auditoria taller

---
+++ ProyectoLenguajes/ProyectoLenguajes/Auditoria_Taller.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ProyectoLenguajes
+{
+    partial class Auditoria_Taller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.AuditbicisGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.AuditbicisGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.Tan;
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Location = new System.Drawing.Point(832, 31);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(126, 57);
+            this.btnRegresar.TabIndex = 7;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.Tan;
+            this.btnConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.Location = new System.Drawing.Point(8, 31);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(126, 57);
+            this.btnConsultar.TabIndex = 6;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // AuditbicisGrid
+            // 
+            this.AuditbicisGrid.BackgroundColor = System.Drawing.Color.DarkOliveGreen;
+            this.AuditbicisGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AuditbicisGrid.Location = new System.Drawing.Point(186, 155);
+            this.AuditbicisGrid.Name = "AuditbicisGrid";
+            this.AuditbicisGrid.RowHeadersWidth = 58;
+            this.AuditbicisGrid.Size = new System.Drawing.Size(590, 422);
+            this.AuditbicisGrid.TabIndex = 5;
+            // 
+            // Auditoria_Taller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProyectoLenguajes.Properties.Resources.imgBanner;
+            this.ClientSize = new System.Drawing.Size(966, 609);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.AuditbicisGrid);
+            this.Name = "Auditoria_Taller";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auditoria_Taller";
+            ((System.ComponentModel.ISupportInitialize)(this.AuditbicisGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.DataGridView AuditbicisGrid;
+    }
+}
---
+++ ProyectoLenguajes/ProyectoLenguajes/Auditoria_Taller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OracleClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoLenguajes
+{
+    public partial class Auditoria_Taller : Form
+    {
+     
+        public Auditoria_Taller(String usuario_activo)
+        {
+            InitializeComponent();
+            this.usuario_activo = usuario_activo;
+        }
+        String usuario_activo;
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            OracleConnection conexion = new OracleConnection("DATA SOURCE = ORCL; PASSWORD =DBFide1; USER ID = system;");
+            conexion.Open();
+            OracleCommand comando = new OracleCommand("seleccionarTallerAudits", conexion);
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.Parameters.Add("audit_taller", OracleType.Cursor).Direction = ParameterDirection.Output;
+
+            OracleDataAdapter adaptador2 = new OracleDataAdapter();
+            adaptador2.SelectCommand = comando;
+            DataTable tabladeBicis = new DataTable();
+            adaptador2.Fill(tabladeBicis);
+            AuditbicisGrid.DataSource = tabladeBicis;
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            Auditorias audits = new Auditorias(usuario_activo);
+            audits.Show();
+            this.Hide();
+        }
+    }
+}
