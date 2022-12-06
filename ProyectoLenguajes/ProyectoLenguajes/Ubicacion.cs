576f0e92e3f7d5e644d639717c91c7d65aaf8a9e
dannyrg95 <dannyrg-@hotmail.com>
2022-12-05 21:48:30 -0600

    Correccion Ubicacion.cs

--- ProyectoLenguajes/ProyectoLenguajes/Ubicacion.cs
+++ ProyectoLenguajes/ProyectoLenguajes/Ubicacion.cs
@@ -13,13 +13,12 @@ using System.Windows.Forms;
 namespace ProyectoLenguajes
 {
     public partial class Ubicacion : Form
-        
+
     {
         OracleConnection conexion = new OracleConnection("DATA SOURCE = ORCL; PASSWORD = DBFide1; USER ID = system;");
 
         public Ubicacion(String usuario_activo)
         {
-            InitializeComponent();
             InitializeComponent();
             this.usuario_activo = usuario_activo;
         }
@@ -28,42 +27,48 @@ namespace ProyectoLenguajes
         String usuario_activo;
 
         private void button1_Click(object sender, EventArgs e)
-
-        {
-
-        }
-
-        private void gMap_Load(object sender, EventArgs e)
         {
             gMap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance; //Establece proveedor de mapas
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly; //Configura en solo datos del servidor
+            //gMap.SetPositionByKeywords("San Jose, Costa Rica"); //Establecer Ciudad, Pais
+            //gMap.Position = new GMap.NET.PointLatLng(9.9363147, -84.0790631); //Coordenadas de Ciclo Boutique CR
             gMap.SetPositionByKeywords("Guanacaste, Costa Rica"); //Establecer Ciudad, Pais
             double latitud = 0.0;
             double longitud = 0.0;
-           try
+            try
             {
 
                 conexion.Open();
-            OracleCommand comando = new OracleCommand("seleccionarCoordenadas", conexion);
-            comando.CommandType = System.Data.CommandType.StoredProcedure;
-            
-            comando.Parameters.Add("filas_coordenadas", OracleType.Cursor).Direction = ParameterDirection.Output;
-            comando.ExecuteNonQuery();
-            OracleDataReader registro = comando.ExecuteReader();
-            if (registro.Read())
-            {
+                OracleCommand comando = new OracleCommand("seleccionarCoordenadas", conexion);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
 
-                latitud = double.Parse(registro["latitud"].ToString());
-                longitud = double.Parse(registro["longitud"].ToString());
+                comando.Parameters.Add("filas_coordenadas", OracleType.Cursor).Direction = ParameterDirection.Output;
+                comando.ExecuteNonQuery();
+                OracleDataReader registro = comando.ExecuteReader();
+                if (registro.Read())
+                {
 
+                    latitud = double.Parse(registro["latitud"].ToString());
+                    longitud = double.Parse(registro["longitud"].ToString());
+
+                }
             }
-            }catch(Exception)
+            catch (Exception)
             {
                 MessageBox.Show("Error");
             }
             conexion.Close();
             // gMap.Position = new GMap.NET.PointLatLng(latitud, longitud); //Coordenadas de Ciclo Boutique CR
             gMap.Position = new PointLatLng(latitud, longitud);
+            gMap.ShowCenter = false; //Eliminar + Rojo en gMap
+        }
+
+        private void gMap_Load(object sender, EventArgs e)
+        {
+            gMap.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance; //Establece proveedor de mapas
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly; //Configura en solo datos del servidor
+            gMap.SetPositionByKeywords("San Jose, Costa Rica"); //Establecer Ciudad, Pais
+            gMap.Position = new GMap.NET.PointLatLng(9.9363147, -84.0790631); //Coordenadas de Ciclo Boutique CR
             gMap.ShowCenter = false; //Eliminar + Rojo en gMap
         }
 
